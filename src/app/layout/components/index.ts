a04aaaf1704afb9eaafdb50bfbf331fd2f12512b
hengloem <hengloem.pnc@gmail.com>
2023-10-24 16:28:56 +0700

    Removed unsued components and fixed bug

--- src/app/layout/components/index.ts
+++ src/app/layout/components/index.ts
@@ -1,37 +1,28 @@
-import { FooterComponent } from "./footer/footer.component";
 import { ForgetpwdComponent } from "./forgetpwd/forgetpwd.component";
 import { HeaderComponent } from "./header/header.component";
 import { LoadingComponent } from "./loading/loading.component";
 import { LoginComponent } from "./login/login.component";
 import { NavbarLeftComponent } from "./navbar-left/navbar-left.component";
-import { NavbarRightComponent } from "./navbar-right/navbar-right.component";
 import { NavigationComponent } from "./navigation/navigation.component";
 import { Page404Component } from "./page404/page404.component";
 import { ProfileComponent } from "./profile/profile.component";
-import { RegisterComponent } from "./register/register.component";
 
 export const COMPONENTS = [
-    FooterComponent,
     ForgetpwdComponent,
     HeaderComponent,
     LoginComponent,
     NavbarLeftComponent,
-    NavbarRightComponent,
     Page404Component,
-    RegisterComponent,
     LoadingComponent,
     ProfileComponent,
     NavigationComponent
 ];
 
-export * from "./footer/footer.component";
 export * from "./forgetpwd/forgetpwd.component";
 export * from "./header/header.component";
 export * from "./login/login.component";
 export * from "./navbar-left/navbar-left.component";
-export * from "./navbar-right/navbar-right.component";
 export * from "./page404/page404.component";
-export * from "./register/register.component";
 export * from "./loading/loading.component";
 export * from "./profile/profile.component";
 export * from "./navigation/navigation.component";
