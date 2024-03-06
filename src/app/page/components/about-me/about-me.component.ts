94b020b75bd7172979c7f7ecc509ada91457fff6
hengloem <hengloem.pnc@gmail.com>
2024-03-07 00:42:22 +0700

    Update age

--- src/app/page/components/about-me/about-me.component.ts
+++ src/app/page/components/about-me/about-me.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
+  ageData: { years: number, suffix: string };
 
   constructor() { }
 
   ngOnInit(): void {
+    this.calculateAge()
+
   }
 
+  calculateAge(): { years: number, suffix: string } {
+    const birthDate: Date = new Date('1995-05-07');
+    const currentDate: Date = new Date();
+
+    // Calculate the difference in milliseconds
+    const difference = currentDate.getTime() - birthDate.getTime();
+
+    // Convert the difference to years
+    const years = Math.floor(difference / (1000 * 60 * 60 * 24 * 365.25));
+    
+    // Determine the suffix based on the age
+    const suffix = years === 1 ? 'year old' : 'years old';
+    
+    return { years, suffix };
+  }
 }
