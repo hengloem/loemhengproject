393f35d10b2d2c3e913a70393fb36352cf9b605e
hengloem <hengloem.pnc@gmail.com>
2024-03-06 22:39:15 +0700

    Remove vercel package.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { SharedService } from './core/services/shared.service';
 import { NavigationEnd, Router } from '@angular/router';
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 @Component({
   selector: 'app-root',
