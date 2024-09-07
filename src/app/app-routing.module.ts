2c779506d406be7c669c0f7cf8e9cfb130a76cba
hengloem <hengloem.pnc@gmail.com>
2024-09-07 09:41:03 +0700

    Modify performance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,55 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './page/components/home/home.component';
-import { Page404Component } from './layout/components';
-import { AboutMeComponent } from './page/components/about-me/about-me.component';
-import { PortfolioComponent } from './page/components/portfolio/portfolio.component';
-import { BlogComponent } from './page/components/blog/blog.component';
-import { ContactMeComponent } from './page/components/contact-me/contact-me.component';
-import { BlogPostComponent } from './page/components';
+
+import { HomeComponent } from './page/home/home.component';
+import { AboutMeComponent } from './page/about-me/about-me.component';
+import { PortfolioComponent } from './page/portfolio/portfolio.component';
+import { BlogComponent } from './page/blog/blog.component';
+import { ContactMeComponent } from './page/contact-me/contact-me.component';
+import { BlogPostComponent } from './page/blog-post/blog-post.component';
+import { Page404Component } from './layout/page404/page404.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'about',
-    component: AboutMeComponent
-  },
-  {
-    path: 'portfolio',
-    component: PortfolioComponent
-  },
-  {
-    path: 'contact',
-    component: ContactMeComponent
-  },
-  {
-    path: 'blog',
-    component: BlogComponent
-  },
-  {
-    path: 'sblog-post',
-    component: BlogPostComponent
-  },
-  {
-    path: '**',
-    component: Page404Component
-  }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutMeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'contact', component: ContactMeComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'blog-post', component: BlogPostComponent },
+  { path: '**', component: Page404Component } // single wildcard for 404 page
 ];
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
     RouterModule.forRoot(routes)
-  ]
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
