2ed1ae7c93e9f37c4140d175b161d6cb9199c92a
hengloem <hengloem.pnc@gmail.com>
2024-10-28 16:59:15 +0700

    Change khmer font and update defult lang

---
+++ src/app/core/services/language-service.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LanguageServiceService {
+  private currentLang: string;
+
+  constructor(private translate: TranslateService) {}
+
+  // Method to set language
+  setLanguage(lang: string): void {
+    this.currentLang = lang;
+    this.translate.use(lang);
+  }
+
+  // Method to get current language
+  getLanguage(): string {
+    return this.currentLang;
+  }
+}
