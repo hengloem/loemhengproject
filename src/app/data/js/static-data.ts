b0726775f0ed6449e788669b64aedd3c5929bd66
hengloem <hengloem.pnc@gmail.com>
2024-11-01 15:46:08 +0700

    Update calculator home

--- src/app/data/js/static-data.ts
+++ src/app/data/js/static-data.ts
@@ -1,10 +1,10 @@
 export const MENUS = [
-    { title: 'Home', icon: 'fa fa-home', route: '/home' },
-    { title: 'About', icon: 'fa fa-user', route: '/about' },
-    { title: 'Portfolio', icon: 'fa fa-briefcase', route: '/portfolio' },
-    { title: 'Contact', icon: 'fa fa-envelope-open', route: '/contact' },
-    { title: 'Blog', icon: 'fa fa-comments', route: '/blog' },
-    { title: 'Tool', icon: 'fa fa-smile-o', route: '/math-tools' }
+    { title: 'app.home', icon: 'fa fa-home', route: '/home' },
+    { title: 'app.about', icon: 'fa fa-user', route: '/about' },
+    { title: 'app.portfolio', icon: 'fa fa-briefcase', route: '/portfolio' },
+    { title: 'app.contact', icon: 'fa fa-envelope-open', route: '/contact' },
+    { title: 'app.blog', icon: 'fa fa-comments', route: '/blog' },
+    { title: 'app.tool', icon: 'fa fa-smile-o', route: '/math-tools' }
 ];
 
 export const aboutMe = {
