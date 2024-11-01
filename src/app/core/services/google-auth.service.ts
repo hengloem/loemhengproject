7ffa3e1841300919f7e8ecb33a428aa606d14427
hengloem <hengloem.pnc@gmail.com>
2024-11-01 17:09:53 +0700

    Update api

---
+++ src/app/core/services/google-auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { environment } from '@env';
+import { loadGapiInsideDOM, gapi } from 'gapi-script';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GoogleAuthService {
+  private gapiSetup: boolean = false;
+  private authInstance: gapi.auth2.GoogleAuth;
+
+  constructor() {
+    this.initGapiClient();
+  }
+
+  async initGapiClient() {
+    await loadGapiInsideDOM();
+    gapi.load('client:auth2', async () => {
+      await gapi.client.init({
+        clientId: environment.googleClientId,
+        scope: 'https://www.googleapis.com/auth/blogger',
+      });
+      this.authInstance = gapi.auth2.getAuthInstance();
+      this.gapiSetup = true;
+    });
+  }
+
+  signIn() {
+    return this.authInstance.signIn();
+  }
+
+  signOut() {
+    return this.authInstance.signOut();
+  }
+}
