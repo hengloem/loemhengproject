974ffe7a889cdfbaebe32ebb015a7bff15261365
hengloem <hengloem.pnc@gmail.com>
2024-12-27 03:24:39 +0700

    Updated about me

--- src/app/page/skill/skill.component.ts
+++ src/app/page/skill/skill.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { lh_ratingSkills } from '@app/data/js/static-data';
 
 @Component({
   selector: 'app-skill',
   templateUrl: './skill.component.html',
   styleUrls: ['./skill.component.scss']
 })
-export class SkillComponent {
+export class SkillComponent implements OnInit {
+  lh_skillData: any = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.lh_skillData = lh_ratingSkills;
+  }
 
 }
---
+++ src/vercel-analytics.d.ts
@@ -0,0 +1,4 @@
+declare module '@vercel/analytics' {
+    const analytics: any;
+    export default analytics;
+}
